41b89d0cebbd70e6601a2cdd3e950b2e4432df04
Phil <182430206+Fico27@users.noreply.github.com>
2025-06-04 18:55:19 +0200

    added in generalInfo form.

---
+++ src/components/cv.jsx
@@ -0,0 +1,11 @@
+function Cv({ generalInfo }) {
+  return (
+    <>
+      <h1>{generalInfo.name}</h1>
+      <h1>{generalInfo.email}</h1>
+      <h1>{generalInfo.phone}</h1>
+    </>
+  );
+}
+
+export default Cv;
--- src/components/generalInfo.jsx
+++ src/components/generalInfo.jsx
@@ -1,7 +1,41 @@
-function GeneralInfo() {
+import { useState } from "react";
+
+function GeneralInfo({ generalInfo, setGeneralInfo }) {
   return (
     <div className="genFormContainer">
-      <form action="">i</form>
+      <form action="">
+        <h2>General Information</h2>
+
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => {
+            setGeneralInfo({ ...generalInfo, name: e.target.value });
+          }}
+        />
+
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={(e) => {
+            setGeneralInfo({ ...generalInfo, email: e.target.value });
+          }}
+        />
+
+        <label htmlFor="phone">Phone #: </label>
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          onChange={(e) => {
+            setGeneralInfo({ ...generalInfo, phone: e.target.value });
+          }}
+        />
+      </form>
     </div>
   );
 }
