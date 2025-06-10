e6d2f971ca91d816e7f1e6e95f16999ea1605b3b
Phil <182430206+Fico27@users.noreply.github.com>
2025-06-10 13:08:57 +0200

    Styled experience section.

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -10,8 +10,41 @@ function Cv({ generalInfo, educationInfo, experienceInfo }) {
         </div>
       </header>
 
-      <section className="educationHolder"></section>
-      <section className="experienceHolder"></section>
+      <section className="educationHolder sectionHolder">
+        <div className="titleHolder">
+          <h3>Education</h3>
+        </div>
+        <div className="educationInfoHolder">
+          <div className="dateHolder">
+            <p>
+              {educationInfo.start} - {educationInfo.end}
+            </p>
+          </div>
+          <div className="schoolHolder">
+            <b>{educationInfo.schoolName}</b>
+            <p>{educationInfo.degree}</p>
+          </div>
+        </div>
+      </section>
+      <section className="experienceHolder sectionHolder">
+        <div className="titleHolder">
+          <h3>Experience</h3>
+        </div>
+        <div className="experienceInfoHolder ">
+          <div className="experienceInfo">
+            <div className="company">
+              <b>{experienceInfo.company}</b>
+              <p>{experienceInfo.role}</p>
+            </div>
+            <b>
+              {experienceInfo.start} - {experienceInfo.end}
+            </b>
+          </div>
+          <div className="experienceInfoDes">
+            <p>{experienceInfo.description}</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
